c7799964b80c02be2c3475d84ac50e5bff5f38c8
raoul-g <raoul08@web.de>
2021-06-09 09:43:13 +0200

    Upgrade server to use HTTPS and basic auth

--- sla-manager/src/main.ts
+++ sla-manager/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const httpsOptions = {
+    key: fs.readFileSync('./config/certificate/key.pem'),
+    cert: fs.readFileSync('./config/certificate/cert.pem'),
+  };
+  const rawData =  fs.readFileSync('./config/credentials/basic-user.json');
+  const basicAuthUser = JSON.parse(rawData.toString());
+  
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
+
+  app.use(basicAuth(basicAuthUser));
+
+
+
   await app.listen(6400); // set 80 to directly access from fargate (was 6400)
 }
 bootstrap();
