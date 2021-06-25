2f638744edcbb6281730204e2576e28571677724
Jonas Wallmann <jonaswallmann@hotmail.de>
2021-06-25 11:48:01 +0200

    frontend api auth

--- solomon-frontend/src/api.tsx
+++ solomon-frontend/src/api.tsx
@@ -3,14 +3,19 @@ import { DeploymentEnvironment, SloRule, Target } from "solomon-models/";
 
 
 const BACKEND_URL = "https://localhost:443/solomon";
-// const BACKEND_URL = "http://localhost:6400/solomon";
 const RULES_API = `${BACKEND_URL}/rules`; // :deploymentEnvironment
 const TARGET_API = `${BACKEND_URL}/targets`; // :deploymentEnvironment
 const GROPIUS_COMPONENTS_API = `${BACKEND_URL}/gropius-projects`; // :gropiusProjectId
 
+const axiosConfig = {
+    auth: {
+        username: "yourusername",
+        password: "yourpassword",
+    }
+}
 
 export const fetchRules = async (env: DeploymentEnvironment): Promise<SloRule[]> => {
-    const res = await Axios.get<SloRule[]>(`${RULES_API}/${env}`);
+    const res = await Axios.get<SloRule[]>(`${RULES_API}/${env}`, axiosConfig);
     if (res.data as any === "") {   // why does api return ""?
         return null;
     }
@@ -24,12 +29,12 @@ export const fetchRules = async (env: DeploymentEnvironment): Promise<SloRule[]>
 // }
 
 export const postRule = async (rule: SloRule) => {
-    const res = await Axios.post(`${RULES_API}`, rule);
+    const res = await Axios.post(`${RULES_API}`, rule, axiosConfig);
     return res.data;
 }
 
 export const fetchTargets = async (env: DeploymentEnvironment): Promise<Target[]> => {
-    const res = await Axios.get(`${TARGET_API}/${env}`);
+    const res = await Axios.get(`${TARGET_API}/${env}`, axiosConfig);
     if (res.data as any === "") {   // why does api return ""?
         return null;
     }
@@ -37,6 +42,6 @@ export const fetchTargets = async (env: DeploymentEnvironment): Promise<Target[]
 }
 
 export const fetchGropiusComponents = async (gropiusProjectId: string): Promise<any[]> => {
-    const res = await Axios.get(`${GROPIUS_COMPONENTS_API}/${gropiusProjectId}`);
+    const res = await Axios.get(`${GROPIUS_COMPONENTS_API}/${gropiusProjectId}`, axiosConfig);
     return res.data;
 }
