00a6297175ff1b8f0b73c33c98b2340b1b7fd4e7
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2021-07-19 15:00:47 +0200

    Removed solomon-models

--- solomon-models/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Target } from "./target.model";
-import SloRule, { DeploymentEnvironment, MetricOption, ComparisonOperator, StatisticsOption, PresetOption } from "./slo-rule.model";
-export { SloRule, DeploymentEnvironment, MetricOption, ComparisonOperator, StatisticsOption, PresetOption, Target };
